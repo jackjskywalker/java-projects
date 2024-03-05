fd9d53d891c6090f7f9c956ea27f4dd96971a8c5
jackjskywalker <jackjskywalker@yahoo.com>
2024-03-04 22:32:03 -0500

    Controller Format

--- AddressBook/src/app/Controller.java
+++ AddressBook/src/app/Controller.java
@@ -13,67 +13,73 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     AddressBook AD;
-
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        AD = new AddressBook();
-    }
+    public void initialize(URL url, ResourceBundle rb) { AD = new AddressBook(); }
+
+    // Table Content Elements
+
     @FXML
     private TableView<?> addressBookTable;
-
-    @FXML
-    private TableColumn<?, ?> addressColumn;
-
-    @FXML
-    private TextField addressInputField;
-
-    @FXML
-    private TableColumn<?, ?> birthdayColumn;
-
-    @FXML
-    private TextField birthdayInputField;
-
-    @FXML
-    private TableColumn<?, ?> cityColumn;
-
-    @FXML
-    private TextField cityInputField;
-
-    @FXML
-    private Button clearButton;
-
-    @FXML
-    private Button deleteButton;
-
-    @FXML
-    private Button findButton;
 
     @FXML
     private TableColumn<?, ?> firstNameColumn;
 
     @FXML
-    private TextField firstNameInputField;
-
-    @FXML
     private TableColumn<?, ?> lastNameColumn;
 
     @FXML
-    private TextField lastNameInputField;
+    private TableColumn<?, ?> birthdayColumn;
+
+    @FXML
+    private TableColumn<?, ?> addressColumn;
+
+    @FXML
+    private TableColumn<?, ?> cityColumn;
 
     @FXML
     private TableColumn<?, ?> stateColumn;
 
     @FXML
+    private TableColumn<?, ?> zipColumn;
+
+    // Entry Input Elements
+
+    @FXML
+    private TextField firstNameInputField;
+
+    @FXML
+    private TextField lastNameInputField;
+
+    @FXML
+    private TextField birthdayInputField;
+
+    @FXML
+    private TextField addressInputField;
+
+    @FXML
+    private TextField cityInputField;
+
+    @FXML
     private TextField stateInputField;
+
+    @FXML
+    private TextField zipInputField;
+
+    // Button Elements
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private Button findButton;
 
     @FXML
     private Button updateButton;
 
     @FXML
-    private TableColumn<?, ?> zipColumn;
+    private Button deleteButton;
 
-    @FXML
-    private TextField zipInputField;
+    // Action Elements
 
     @FXML
     void clearEntry(ActionEvent event) {
