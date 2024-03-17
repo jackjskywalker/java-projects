feb1409cc997b2271ddad8e21d0ee9de06743d7d
jackjskywalker <jackjskywalker@yahoo.com>
2024-03-17 17:42:50 -0400

    Update Controller.java

--- AddressBook/src/app/Controller.java
+++ AddressBook/src/app/Controller.java
@@ -23,7 +23,6 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
