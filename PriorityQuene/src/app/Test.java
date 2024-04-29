6316f750b8470e00eaa809da5895dcf9ba59a2b4
benaturner <bent30070@gmail.com>
2024-04-29 19:53:38 -0400

    added honesty statements

--- PriorityQuene/src/app/Test.java
+++ PriorityQuene/src/app/Test.java
@@ -1,5 +1,15 @@
 package app;
 
+/*
+- I have not discussed the source code in my program with anyone other than my instructor’s approved human sources (i.e. programming partner).
+
+- I have not used source code obtained from another student, or any other unauthorized source, either modified or unmodified.
+
+- If any source code or documentation used in my program was obtained from another source, such as a text book or course notes, that has been clearly noted with a proper citation in the comments of my program.
+
+- I have not knowingly designed this program in such a way as to defeat or interfere with the normal operation of any machine it is graded on or to produce apparently correct results when in fact it does not.
+ */
+
 public class Test {
     public static void main(String[] args) {
 
