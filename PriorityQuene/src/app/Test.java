498bb840f9ba36e45b0031e91b51a2458c589f4d
jackjskywalker <jackjskywalker@yahoo.com>
2024-04-29 20:40:37 -0400

    commit

--- PriorityQuene/src/app/Test.java
+++ PriorityQuene/src/app/Test.java
@@ -1,3 +1,12 @@
+/**
+ * CSIS312 Assignment 8 - Priority Quene
+ * @author Benjamin Turner and Jack Skywalker
+ * - I have not discussed the source code in my program with anyone other than my instructor’s approved human sources (i.e. programming partner).
+ * - I have not used source code obtained from another student, or any other unauthorized source, either modified or unmodified.
+ * - If any source code or documentation used in my program was obtained from another source, such as a text book or course notes, that has been clearly noted with a proper citation in the comments of my program.
+ * - I have not knowingly designed this program in such a way as to defeat or interfere with the normal operation of any machine it is graded on or to produce apparently correct results when in fact it does not.
+ */
+
 package app;
 
 /*
